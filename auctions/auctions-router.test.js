9fe3742d636f7a622d6083d8884fc6474a039236
Logan Sorensen <loganmsorensen@gmail.com>
2020-03-05 17:36:39 -0800

    added auth tests

--- auctions/auctions-router.test.js
+++ auctions/auctions-router.test.js
@@ -2,7 +2,19 @@ const request = require("supertest");
 
 const server = require("../api/server.js");
 
-describe("bids router", function() {
+let token;
+
+beforeAll(done => {
+  request(server)
+    .post("/api/auth/login")
+    .send({ username: "new user6", password: "password" })
+    .then(res => {
+      token = res.body.token;
+      done();
+    });
+});
+
+describe("auctions router", function() {
   it("should run the tests", function() {
     expect(true).toBe(true);
   });
@@ -11,6 +23,48 @@ describe("bids router", function() {
     it("should return a 400 error", function() {
       return request(server)
         .get("/api/auctions")
+        .then(res => {
+          expect(res.status).toBe(400);
+        });
+    });
+
+    it("should return a 200 OK", function() {
+      return request(server)
+        .get("/api/auctions")
+        .set("Authorization", token)
+        .then(res => {
+          expect(res.status).toBe(200);
+        });
+    });
+  });
+
+  describe("POST /", function() {
+    it("should return a 201 created", function() {
+      return request(server)
+        .post("/api/auctions")
+        .set("Authorization", token)
+        .send({
+          name: "test",
+          image: "test",
+          description: "test",
+          starting_price: 100,
+          deadline: "test"
+        })
+        .then(res => {
+          expect(res.status).toBe(201);
+        });
+    });
+
+    it("should return a 400 error", function() {
+      return request(server)
+        .post("/api/auctions")
+        .send({
+          name: "test",
+          image: "test",
+          description: "test",
+          starting_price: 100,
+          deadline: "test"
+        })
         .then(res => {
           expect(res.status).toBe(400);
         });
--- bids/bids-router.test.js
+++ bids/bids-router.test.js
@@ -2,6 +2,18 @@ const request = require("supertest");
 
 const server = require("../api/server.js");
 
+let token;
+
+beforeAll(done => {
+  request(server)
+    .post("/api/auth/login")
+    .send({ username: "new user", password: "password" })
+    .then(res => {
+      token = res.body.token;
+      done();
+    });
+});
+
 describe("bids router", function() {
   it("should run the tests", function() {
     expect(true).toBe(true);
@@ -15,5 +27,34 @@ describe("bids router", function() {
           expect(res.status).toBe(400);
         });
     });
+
+    it("should return 200 OK", function() {
+      return request(server)
+        .get("/api/bids")
+        .set("Authorization", token)
+        .then(res => {
+          expect(res.status).toBe(200);
+        });
+    });
+  });
+
+  describe("POST /:id", function() {
+    it("should return a 400 error", function() {
+      return request(server)
+        .post("/api/bids")
+        .then(res => {
+          expect(res.status).toBe(400);
+        });
+    });
+
+    // it("should return a 404 error", function() {
+    //   return request(server)
+    //     .post("/api/bids/200")
+    //     .set("Authorization", token)
+    //     .send({ amount: 500 })
+    //     .then(res => {
+    //       expect(res.status).toBe(404);
+    //     });
+    // });
   });
 });
